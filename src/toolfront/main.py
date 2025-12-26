0ecaf8609d55b4bfec43aa110bd918fde4da67cf
Esteban Safranchik <safranchik.e@gmail.com>
2025-12-26 12:10:11 -0300

    cli: remove MCP

--- src/toolfront/main.py
+++ src/toolfront/main.py
@@ -3,7 +3,6 @@ import click
 from .cli.delete import delete
 from .cli.deploy import deploy
 from .cli.list import list_deployments
-from .cli.mcp import mcp
 from .cli.serve import serve
 from .cli.tokens import tokens
 from .cli.update import update
@@ -16,7 +15,6 @@ def main():
 
 
 main.add_command(serve)
-main.add_command(mcp)
 main.add_command(deploy)
 main.add_command(list_deployments, name="list")
 main.add_command(update)
