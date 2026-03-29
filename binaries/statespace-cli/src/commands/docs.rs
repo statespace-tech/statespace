a8cbdf3c5f925ea453f1d66d84877c9066174896
Esteban Safranchik <safranchik.e@gmail.com>
2026-03-29 10:34:05 -0700

    chore: remove docs command (replaced by guide)

--- binaries/statespace-cli/src/commands/docs.rs
+++
@@ -1,8 +0,0 @@
-use crate::error::{Error, Result};
-
-const DOCS_URL: &str = "https://docs.statespace.com";
-
-pub(crate) fn run_docs() -> Result<()> {
-    eprintln!("Opening {DOCS_URL}...");
-    open::that(DOCS_URL).map_err(|e| Error::cli(format!("failed to open browser: {e}")))
-}
